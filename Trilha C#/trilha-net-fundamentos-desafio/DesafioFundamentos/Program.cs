184de1c64ff832e22b9b137b4bc1b63f7dbadded
Guilherme Xavier <guilherme.xavier27@outlook.com>
2025-10-31 15:52:11 -0300

    Commit Desafio trilha dotnet fundamenos

---
+++ Trilha C#/trilha-net-fundamentos-desafio/DesafioFundamentos/Program.cs
@@ -0,0 +1,50 @@
+﻿using DesafioFundamentos.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento! \nDigite o preço inicial:");
+decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+var es = new Estacionamento(precoInicial, precoPorHora);
+
+var exibirMenu = true;
+
+// menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+        case "2":
+            es.RemoverVeiculo();
+            break;
+        case "3":
+            es.ListarVeiculos();
+            break;
+        case "4":
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
