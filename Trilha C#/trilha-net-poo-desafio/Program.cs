36722c5f66bd49455d6570ad889b56b6dc3b09d0
Guilherme Xavier <guilherme.xavier27@outlook.com>
2025-10-31 19:18:03 -0300

    Projeto sistema celular poo

---
+++ Trilha C#/trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("SmartPhone Nokia: ");
+var nokia = new Nokia(numero: "1234", modelo: "AX2055", iMEI: "111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo(nomeApp: "whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Iphone");
+var iphone = new Iphone(numero: "145688", modelo: "iphone6", iMEI: "22222", memoria: 84);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo(nomeApp: "instagram");
